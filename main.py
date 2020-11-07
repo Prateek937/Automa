c7c5dbb8d71aedce4ccc8a4f188f568a076965d4
root <root@localhost.localdomain>
2020-11-07 18:09:18 +0530

    added docker_install()

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import subprocess as sb
 
-def hadoop():
-    out = sb.getstatusoutput("echo 'Y' | hadoop namenode -format")
-    print(out[0])
-    out2 = sb.getstatusoutput("rpm -q httpd")
-    print(out[0])
+def docker_install():
+	output = sb.getstatusoutput('rpm -q docker')
+	if output[0] == 0:
+		if output == 'package docker is not installed':
+			sb.getstatusoutput('wget https://download.docker.com/linux/centos/7/x86_64/stable/')
+			outd = sb.getstatusoutput('yum install docker-ce --nobest')
+			if outd[0] == 0:
+				listd = outd[1].split("\n")
+				if listd[-1] == 'complete':
+					return 'Docker-ce successfully installed !'
+				else:
+					return 'Not installed !', outd[1]
+		else:
+			return 'Docker-ce already installed !'
 
-hadoop()
+out = list(docker_install())
+for x in out:
+	print(x)
